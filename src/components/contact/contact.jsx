6f787e98bd8df4d463a82d2ba5956ee595c36f5a
Efuet Roderic <efuetroderic@gmail.com>
2023-06-06 18:16:23 -0700

    fourth commit

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -3,12 +3,12 @@ import './contact.css'
 import{MdOutlineEmail} from 'react-icons/md'
 import{BsWhatsapp} from 'react-icons/bs'
 import {BsInstagram} from 'react-icons/bs'
-import{ useRef } from 'react';
+import{ useRef } from 'react'
 import emailjs from 'emailjs-com'
 
 
 const contact = () => {
-    const form = useRef();
+   const form = useRef();
 
     const sendEmail = (e) => {
         e.preventDefault();
@@ -28,21 +28,21 @@ const contact = () => {
            <center> <MdOutlineEmail className='contact_option-icon'/></center>
             <h4><center>Email</center></h4>
             <h5>efuetroderic@gmail.com</h5>
-            <a href="mailto:efuetroderic@gmail.com" target="_blank">Send a message </a>
+            <a href="mailto:efuetroderic@gmail.com" target="_blank" rel = "noreferrer">Send a message </a>
             </article> 
            
             <article className='contact_options'>
             <center><BsWhatsapp className='contact_option-icon'/></center>
             <h4> <center>WhatsApp</center></h4>
             <h5>whatsapp Number</h5>
-            <a href="https://api.whatsApp.com/send?phone+237650667583 " target="_blank">Send a message </a>
+            <a href="https://api.whatsApp.com/send?phone+237650667583 " target="_blank" rel = "noreferrer">Send a message </a>
             </article> 
 
             <article className='contact_options'>
             <center><BsInstagram className='contact_option-icon'/></center>
             <h4> <center>Instagram</center></h4>
             <h5>Instagram Account</h5>
-            <a href="https://m.me/efuetroderic" target="_blank">Send a message </a>
+            <a href="https://m.me/efuetroderic" target="_blank" rel = "noreferrer">Send a message </a>
             </article> 
         </div>
 
@@ -50,7 +50,7 @@ const contact = () => {
             <input type="text" name='name' placeholder='Your Full Name' required/>
             <input type="email" name='email' placeholder='Your Email' required/>
             <textarea name="message"rows="7" placeholder='Your Message' required></textarea>
-            <button type='summit' className='btn btn-primary'>Testified</button>
+            <button type='summit' className='btn btn-primary'>Send</button>
         </form>
     </div>
   </section>
